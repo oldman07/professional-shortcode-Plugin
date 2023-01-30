3742aa2055f0d45a85f04e9ace8992545415b310
oldman07 <backagain010@gmail.com>
2023-01-30 16:25:47 +0500

    plugin start setup

---
+++ professional_shortcode.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name:   Professional ShortCode
+* Plugin URI:    https://tutsplus.com/
+* Description:   Adds a call to action box
+* Version:       1.0
+* Author:        Rachel McCollin
+* Author URI:    http://rachelmccollin.com
+*
+*
+*/
